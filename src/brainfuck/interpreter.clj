12d832c73f6a8240fa7bea775ece54abdb0e9f83
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-07-17 00:53:53 +0530

    Looping works this is awesome

--- src/brainfuck/interpreter.clj
+++ src/brainfuck/interpreter.clj
@@ -14,10 +14,6 @@
              >+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.
              +++.------.--------.>>+.>++.")
 
-(def src "++++++++>++++>++>+++>+++>+<<<<-
-             >+>+>->>+<<->>.>---.+++++++..+++.>>.<-.<.
-             +++.------.--------.>>+.>++.")
-
 (defn inc-pointer
   "Increments pointer by 1"
   [{pointer :pointer :as data}]
@@ -31,7 +27,7 @@
 (defn inc-byte
   "Increment byte by 1"
   [{state :state pointer :pointer :as data}]
-  (assoc data :state (assoc state pointer (inc (nth state pointer)))) )
+  (assoc data :state (assoc state pointer (inc (nth state pointer)))))
 
 (defn dec-byte
   "Decrement byte by 1"
@@ -42,13 +38,27 @@
   "Print byte at pointer to console"
   [{state :state pointer :pointer :as data}]
   (do
-    (print (nth state pointer))
+    (print (char (nth state pointer)))
     data))
 
 (defn input-byte
   "Set byte at pointer to user input"
   [{state :state pointer :pointer :as data}]
   (assoc data :state (assoc state pointer (read-line))))
+
+(defn jump-forward
+  "Go to next instruction if data at pointer is not 0 else jump to closest ]"
+  [{:keys [state pointer stack token-counter] :as data}]
+  (if (not= (dec token-counter) (last stack)) 
+    (assoc data :stack (conj stack (dec token-counter)))
+    data))
+
+(defn jump-backward
+  "Go to next instruction if data at pointer is 0 else jump back to closest ["
+  [{:keys [state pointer stack token-counter] :as data}]
+  (if (> (nth state pointer) 0)
+    (assoc data :token-counter (last stack)) 
+    (assoc data :stack (pop stack))))
 
 (defn parse
   [source]
@@ -58,12 +68,12 @@
 (defn interpret
   "Interpret source tokens"
   [tokens]
-  (loop [prog-data {:state (vec (repeat 1042 0)) :pointer 0 :stack [] :token-counter 0}]
+  (loop [prog-data {:state (vec (repeat 15 0)) :pointer 0 :stack [] :token-counter 0}]
     (let [token-counter (:token-counter prog-data)]
-    (if (= token-counter (count tokens)) 
-      prog-data 
-      (recur ((resolve (symbol (nth tokens token-counter))) 
-                  (assoc prog-data :token-counter (inc token-counter))))))))
+      (if (= token-counter (count tokens)) 
+        prog-data 
+        (recur ((resolve (symbol (nth tokens token-counter))) 
+                    (assoc prog-data :token-counter (inc token-counter))))))))
 
 
-(interpret (parse (seq src)))
+(interpret (parse (seq source1)))
