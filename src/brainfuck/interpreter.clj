0b11687a7b364a726ac5397f70275d7ece2aa87c
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-07-16 22:30:19 +0530

    Better arg destructuring

--- src/brainfuck/interpreter.clj
+++ src/brainfuck/interpreter.clj
@@ -14,41 +14,41 @@
              >+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.
              +++.------.--------.>>+.>++.")
 
-(def source "++++++++>++++>++>+++>+++>+<<<<-
+(def src "++++++++>++++>++>+++>+++>+<<<<-
              >+>+>->>+<<->>.>---.+++++++..+++.>>.<-.<.
              +++.------.--------.>>+.>++.")
 
 (defn inc-pointer
   "Increments pointer by 1"
-  [state pointer]
-  {:state state :pointer (inc pointer)})
+  [{pointer :pointer :as data}]
+  (assoc data :pointer (inc pointer)))
 
 (defn dec-pointer
   "Decrements pointer by 1"
-  [state pointer]
-  {:state state :pointer (dec pointer)})
+  [{pointer :pointer :as data}]
+  (assoc data :pointer (dec pointer)))
 
 (defn inc-byte
   "Increment byte by 1"
-  [state pointer]
-  {:state (assoc state pointer (inc (nth state pointer))) :pointer pointer})
+  [{state :state pointer :pointer :as data}]
+  (assoc data :state (assoc state pointer (inc (nth state pointer)))) )
 
 (defn dec-byte
   "Decrement byte by 1"
-  [state pointer]
-  {:state (assoc state pointer (dec (nth state pointer))) :pointer pointer})
+  [{state :state pointer :pointer :as data}]
+  (assoc data :state (assoc state pointer (dec (nth state pointer)))))
 
 (defn output-byte
   "Print byte at pointer to console"
-  [state pointer]
+  [{state :state pointer :pointer :as data}]
   (do
     (print (nth state pointer))
-    {:state state :pointer pointer}))
+    data))
 
 (defn input-byte
   "Set byte at pointer to user input"
-  [state pointer]
-  {:state (assoc state pointer (read-line)) :pointer pointer})
+  [{state :state pointer :pointer :as data}]
+  (assoc data :state (assoc state pointer (read-line))))
 
 (defn parse
   [source]
@@ -58,16 +58,12 @@
 (defn interpret
   "Interpret source tokens"
   [tokens]
-  (let [num-tokens (count tokens)]
-    (loop [data {:state (vec (repeat 1042 0)) :pointer 0}
-           call-stack []
-           token-counter 0]
-      (if (= token-counter num-tokens) 
-        data
-        (recur 
-          ((resolve (symbol (nth tokens token-counter))) (:state data) (:pointer data)) 
-          call-stack 
-          (inc token-counter))))))
+  (loop [prog-data {:state (vec (repeat 1042 0)) :pointer 0 :stack [] :token-counter 0}]
+    (let [token-counter (:token-counter prog-data)]
+    (if (= token-counter (count tokens)) 
+      prog-data 
+      (recur ((resolve (symbol (nth tokens token-counter))) 
+                  (assoc prog-data :token-counter (inc token-counter))))))))
 
 
-(interpret (parse (seq source)))
+(interpret (parse (seq src)))
